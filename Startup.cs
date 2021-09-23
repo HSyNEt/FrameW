ba3f0b2dd744b206c406dce1cfc6fd71b0f11d3c
Ben Jaber <sunghsm@gmail.com>
2021-09-23 08:23:19 +0100

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FrameW.Startup))]
+namespace FrameW
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
